1261b73d544a04673441170ace9a9011d44e814e
AbdoMgdy <abdoman46@hotmail.com>
2020-12-23 13:50:50 +0200

    add breakpoints to fix reponsive

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import { Box, makeStyles, Paper } from "@material-ui/core";
+import {
+  Box,
+  makeStyles,
+  Paper,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
 export default function Layout({ children }) {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
   const useStyles = makeStyles((theme) => ({
     paper: {
-      padding: "2rem",
+      padding: matches ? "0rem" : "2rem",
     },
   }));
-  //   const mockData = [
-  //     [1.5, 2.5],
-  //     [1.8, 3.5],
-  //     [2, 4],
-  //     [1.5, 2.5],
-  //     [1.8, 3.5],
-  //     [2, 4],
-  //     [1.5, 2.5],
-  //     [1.8, 3.5],
-  //     [2, 4],
-  //   ];
   const classes = useStyles();
+  console.log(matches);
   return (
-    <Box width="70%" mx="auto" my="4rem">
+    <Box
+      width={matches ? "100%" : "70%"}
+      mx="auto"
+      my={matches ? "0rem" : "4rem"}
+    >
       <Paper className={classes.paper}>{children}</Paper>
     </Box>
   );
