0ef3cdf3d96ae3262d097336f1be40fe7e49e65c
AbdoMgdy <abdoman46@hotmail.com>
2021-01-19 10:50:52 +0200

    use material ui select

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,99 +1,99 @@
-import React from 'react';
-import {Line} from 'react-chartjs-2';
-import {Container} from '@material-ui/core'
-class Graph extends React.Component{
-state = {
-    name: 'Displacement',
-    state : {
-    labels: this.props.t,
-    datasets: [
+import React from "react";
+import { Line } from "react-chartjs-2";
+import { Container, Select, MenuItem } from "@material-ui/core";
+class Graph extends React.Component {
+  state = {
+    name: "Displacement",
+    state: {
+      labels: this.props.t,
+      datasets: [
         {
+          label: this.props.x,
+          fill: false,
+          lineTension: 0.5,
+          backgroundColor: "rgba(75,192,192,1)",
+          borderColor: "rgba(0,0,0,1)",
+          borderWidth: 2,
+          data: this.props.x,
+        },
+      ],
+    },
+  };
+  handleChange = (e) => {
+    if (e.target.value === "x") {
+      const x = {
+        labels: this.props.t,
+        datasets: [
+          {
             label: this.props.x,
             fill: false,
             lineTension: 0.5,
-            backgroundColor: 'rgba(75,192,192,1)',
-            borderColor: 'rgba(0,0,0,1)',
+            backgroundColor: "rgba(75,192,192,1)",
+            borderColor: "rgba(0,0,0,1)",
             borderWidth: 2,
-            data: this.props.x
-        }
-    ]}
-}
-    handleChange = (e) => {
-        if (e.target.value === 'x'){
-            const x = {labels: this.props.t,
-                datasets: [
-                    {
-                        label: this.props.x,
-                        fill: false,
-                        lineTension: 0.5,
-                        backgroundColor: 'rgba(75,192,192,1)',
-                        borderColor: 'rgba(0,0,0,1)',
-                        borderWidth: 2,
-                        data: this.props.x
-                    }
-                ]}
-            this.setState({name: 'Displacement', state: x});
-        }
-        else if (e.target.value === 'v'){
-            const v = {
-                labels: this.props.t,
-            datasets: [
-                {
-                    label: this.props.v,
-                    fill: false,
-                    lineTension: 0.5,
-                    backgroundColor: 'rgba(75,192,192,1)',
-                    borderColor: 'rgba(0,0,0,1)',
-                    borderWidth: 2,
-                    data: this.props.v
-                }
-            ]
-            }
-            this.setState({name: 'Velocity', state: v});
-        }
-        else if (e.target.value === 'a'){
-            const a = {
-                labels: this.props.t,
-                datasets: [
-                    {
-                        label: this.props.a,
-                        fill: false,
-                        lineTension: 0.5,
-                        backgroundColor: 'rgba(75,192,192,1)',
-                        borderColor: 'rgba(0,0,0,1)',
-                        borderWidth: 2,
-                        data: this.props.a
-                    }
-                ]
-                }
-            this.setState({name: 'Acceleration', state: a});
-        }
+            data: this.props.x,
+          },
+        ],
+      };
+      this.setState({ name: "Displacement", state: x });
+    } else if (e.target.value === "v") {
+      const v = {
+        labels: this.props.t,
+        datasets: [
+          {
+            label: this.props.v,
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: "rgba(75,192,192,1)",
+            borderColor: "rgba(0,0,0,1)",
+            borderWidth: 2,
+            data: this.props.v,
+          },
+        ],
+      };
+      this.setState({ name: "Velocity", state: v });
+    } else if (e.target.value === "a") {
+      const a = {
+        labels: this.props.t,
+        datasets: [
+          {
+            label: this.props.a,
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: "rgba(75,192,192,1)",
+            borderColor: "rgba(0,0,0,1)",
+            borderWidth: 2,
+            data: this.props.a,
+          },
+        ],
+      };
+      this.setState({ name: "Acceleration", state: a });
     }
-    render(){
-        return (
-            <Container maxWidth='lg'>
-                    <select onChange={this.handleChange}>
-                        <option value="x">Displacement</option>
-                        <option value="v">Velocity</option>
-                        <option value="a">Acceleration</option>
-                    </select>
-                    <Line
-                        data={this.state.state}
-                        options={{
-                            title:{
-                            display:true,
-                            text:`${this.state.name} with time`,
-                            fontSize:20
-                            },
-                            legend:{
-                            display:false,
-                            position:'right'
-                            }
-                        }}
-                    />
-            </Container>
-            
-        )   
-    }
+  };
+  render() {
+    return (
+      <Container maxWidth="lg">
+        <Select onChange={this.handleChange}>
+          <MenuItem value="x">Displacement</MenuItem>
+          <MenuItem value="v">Velocity</MenuItem>
+          <MenuItem value="a">Acceleration</MenuItem>
+        </Select>
+        <Line
+          data={this.state.state}
+          options={{
+            title: {
+              display: true,
+              text: `${this.state.name} with time`,
+              fontSize: 20,
+            },
+            legend: {
+              display: false,
+              position: "right",
+            },
+          }}
+        />
+      </Container>
+    );
+  }
 }
 export default Graph;
